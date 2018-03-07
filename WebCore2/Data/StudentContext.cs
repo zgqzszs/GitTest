922c033cbac6012d5dddbedd29a525721b7ca9b6
zgqzszs <914900901@qq.com>
2018-03-07 15:31:37 +0800

    t

---
+++ WebCore2/Data/StudentContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebCore2.Models
+{
+    public class StudentContext : DbContext
+    {
+        public StudentContext (DbContextOptions<StudentContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<WebCore2.Models.Student> Student { get; set; }
+    }
+}
---
+++ WebCore2/Models/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+
+namespace WebCore2.Models
+{
+    public class Student
+    {
+        public int ID { get; set; }
+
+        [StringLength(60, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        [Range(1, 100)]
+        public string Age { get; set; }
+
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
+        public DateTime Birthday { get; set; }
+
+        public void Aafaf()
+        {
+            
+        }
+    }
+}
